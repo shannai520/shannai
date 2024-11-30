3897bd865bb318256c1c08fcb8970ad863dbcf9d
shannai520 <shannaiO-O@outlook.com>
2024-11-30 13:52:43 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -29,7 +29,7 @@ function getBgImg() {
 
 let bg_img_preinstall = {
   type: "1", // 1:默认背景 2:每日一图 3:随机风景 4:随机动漫
-  2: "https://api.dujin.org/bing/1920.php", // 每日一图
+  2: "img/background1.webp", // 每日一图
   3: "https://api.btstu.cn/sjbz/api.php?lx=fengjing&format=images", // 随机风景
   4: "https://www.dmoe.cc/random.php", // 随机动漫
 };
